f6e3b5969c38ee96435248456a3e97a4373a9008
William Marshall <wmarshall3@wisc.edu>
2015-07-06 23:34:17 -0500

    blocksize investigation

---
+++ cusum_blocksize/fourier_cusumBlockSize.m
@@ -0,0 +1,58 @@
+
+
+% Explore ISI for simple oscillator with single phase shift and 
+% iid noise.
+
+% Frequency band is (7, 9)
+
+T = 3000;
+sRate = 250;
+SNR = 0.5;
+freq = 8;
+width = 1;
+phi = 0;
+method = 'resample';
+
+nSim = 250;
+nBoot = 700;
+alpha = 0.05;
+
+list_L = (1:100) * 5;
+nL = length(list_L);
+
+count = zeros(1, nL);
+tau = zeros(1, nL * nSim);
+
+for i = 40:50
+    disp(i)
+    L = list_L(i);
+    for j=1:nSim
+        % Simulate signal with one shift and estimate instantaneous phase
+        x = sim_one_shift(T, sRate, SNR, freq, phi);
+        P = fourier_phase(x, sRate, freq, width);
+        % Burn 
+        Window = round(sRate/(2*width));
+        if mod(Window, 2) == 0
+            Burn = Window / 2;
+        else
+            Burn = (Window - 1) / 2;
+        end
+        P = P((Burn + 1):(end - Burn));
+        [ind, h] = cusum_test(P, L, alpha, nBoot, method);
+        count(i) = count(i) + h;
+        tau((i - 1) * nSim + j) = estimate_tau(P);
+    end
+end
+
+figure()
+
+count = count / nSim;
+L0 = mean(tau) * 2;
+
+plot(list_L, count, 'b', 'linewidth', 2)
+hold on
+plot([0, max(list_L)], [0.05, 0.05], 'k--')
+plot([L0, L0], [0, 1], 'r', 'linewidth', 2)
+
+
+    
--- cusum_blocksize/instant_cusumBlockSize.m
+++ cusum_blocksize/instant_cusumBlockSize.m
@@ -11,16 +11,17 @@ SNR = 0.5;
 freq = 8;
 width = 1;
 phi = 0;
+method = 'resample';
 
-nSim = 500;
-nBoot = 1000;
+nSim = 250;
+nBoot = 700;
 alpha = 0.05;
 
-nL = 100;
-list_L = (1:50) * 10;
+list_L = (1:100) * 5;
+nL = length(list_L);
 
 count = zeros(1, nL);
-tau = zeros(1, nL*nSim);
+tau = zeros(1, nL * nSim);
 
 for i = 1:nL
     disp(i)
@@ -30,25 +31,38 @@ for i = 1:nL
         x = sim_one_shift(T, sRate, SNR, freq, phi);
         P = instant_phase(x, sRate, freq, width);
         % Burn away first 4 seconds
-        P = P(1001:end);
-        [ind, h] = cusum_test(P, L, alpha, nBoot);
+        P = P((2 * sRate):end);
+        [ind, h] = cusum_test(P, L, alpha, nBoot, method);
         count(i) = count(i) + h;
-        tau((i - 1)*nSim + j) = estimate_tau(P);
+        tau((i - 1) * nSim + j) = estimate_tau(P);
     end
 end
 
 figure()
-
 
 count = count / nSim;
 L0 = mean(tau) * 2;
 
 plot(list_L, count, 'b', 'linewidth', 2)
 hold on
-plot([0, sRate], [0.05, 0.05], 'k--')
+plot([0, max(list_L)], [0.05, 0.05], 'k--')
 plot([L0, L0], [0, 1], 'r', 'linewidth', 2)
 
+% Tried a number of different scenarios, all result in 
+% FP rates averaging 10% rather than 5%
 
-            
-    
+% Have increased nSim and list_L
+% Increased T to 20+ seconds
+% Tried permutation and resampling 
+%     - reampling does better but not sufficient
+% Tried 500 and 1000 point burn
+% Tried making L = 2*tau for every signals estimated tau
+
+% To still try:
+% make Width smaller
+% Decrease noise
+% Different initial phi
+% Different frequency?
+% Different alpha
+
     
