415bd4e95dd45b7281bf60e58db3c8e7dbebceb1
William Marshall <wmarshall3@wisc.edu>
2015-07-06 22:26:52 -0500

    Rename permute option to permutation

--- npBoot.m
+++ npBoot.m
@@ -6,10 +6,10 @@ if nargin < 4
 end
 
 if nargin < 5
-    method = 'permute';
+    method = 'resample';
 end
 
-if ~(strcmp(method, 'resample') || strcmp(method, 'permute'))
+if ~(strcmp(method, 'resample') || strcmp(method, 'permutation'))
     print('error: Method must be reample or permute')
     return
 end
@@ -29,13 +29,12 @@ pBlock = reshape(P, block_size, n_blocks);
 for b = 1:nBoot
     if strcmp(method, 'resample')
         Pi = floor(rand([1, n_blocks]) * n_blocks) + 1;
-    elseif strcmp(method, 'permute')
+    elseif strcmp(method, 'permutation')
         Pi = randperm(n_blocks);
     end
     pData = reshape(pBlock(:, Pi), [1, N]);
     C = sqrt(N ./ index ./ (N - index));
     Stat(b) = max(abs(C .* cumsum(pData(1:(N - 1)) - mean(pData))));
-    
 end
 
 Crit = quantile(Stat, 1 - alpha);
