0076269a33aec8e49507fa6b514e491adcf1ac72
slipperyhank <wmarshall3@wisc.edu>
2018-02-08 12:18:00 -0500

    add no_shift function and comment

--- parametric/simulate/no_shifts.m
+++ parametric/simulate/no_shifts.m
@@ -1,39 +1,24 @@
-function x = sim_one_shift(T, sRate, SNR, freq, phi, delta_phi, t0)
+function signal = no_shifts(n_samples, sampling_rate, SNR, frequency, phase)
+% Parametric simulation of an oscillator with no phase shifts.
+% Inputs:
+%   n_samples (int): Length of time series
+%   sampling_rate (int): Samples per second
+%   SNR (float(0,1)): Signal-to-noise ratio (i.i.d Gaussian noise)
+%   frequency (float(0, sampling_rate/2)): Frequency of oscillator
+%   phase (float(0, 2pi)): Constant phase of the oscillator
+%
+% Outputs:
+%   signal (array): simulated observed time series
 
-if nargin < 5
-    error('Not enough parameters')
-elseif nargin < 7
-    delta_phi = 0;
-    t0 = 0;
-end
+% Pure oscillator
+t = 0:(n_samples - 1);
+signal = sin(frequency *2 * pi * t / sampling_rate + phase);
 
-% PARAMETERS
+% Gaussian noise
+epsilon = randn(1, n_samples); 
 
-% T: The number of samples in the signal
-
-% sRate: sampling rate, typically 250 samples per second
-
-% freq: frequency of the ocsillator
- 
-% SNR: singal-to-noise ratio: 1 for no noise
-
-% phi: the initial phase of the oscillator
-% delta_phi: the change in phase of the oscillator
-
-% t0: the temporal index of the change point
-
-t=0:(T-1);
-
-x=sin(freq*2*pi*t/sRate+phi);
-
-if t0 > 0
-    x(t0:end) = sin(freq*2*pi*t(t0:end)/sRate+(phi + delta_phi));
-end
-       
-epsilon = randn(1,T); % normal noise
-%epsilon = rand(1,T); % uniform noise
-
-x = x ./ norm(x);
+% Normalize signals to set SNR
+signal = signal ./ norm(signal);
 epsilon = epsilon ./ norm(epsilon);
 
-x = SNR .* x + (1 - SNR) .* epsilon;
+signal = SNR * signal + (1 - SNR) * epsilon;
