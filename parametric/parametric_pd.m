c64ec71b395c0b6248448133f6853c72198b2978
slipperyhank <wmarshall3@wisc.edu>
2018-02-08 11:13:25 -0500

    combine parametric cusum and pd functions

--- parametric/parametric_pd.m
+++ parametric/parametric_pd.m
@@ -1,9 +1,15 @@
-function [critical_value, statistic] = parametric_cusum_fourier(n_samples, sampling_rate, SNR, frequency, bandwidth, alpha, n_bootstrap, n_burn)
+function [critical_value, statistic] = parametric_pd(n_samples, sampling_rate, SNR, frequency, bandwidth, alpha, n_bootstrap, phase_type, n_burn)
 % Parametric bootstrap of CUSUM test statistics
 % Optional n_burn parameter
 
-if nargin < 7
+if nargin < 8
     n_burn = 0;
+end
+
+if phase_type == 'hilbert'
+    get_phase = @hilbert_phase;
+elseif phase_type == 'fourier'
+    get_phase = @fourier_phase;
 end
 
 n_samples = n_samples + n_burn;
@@ -13,9 +19,9 @@ statistic = zeros(1, n_bootstrap);
 for boot = 1:n_bootstrap
     phi = rand() * 2 * pi;
     bootstrap_signal = sim_one_shift(n_samples, sampling_rate, SNR, frequency, phi);
-    bootstrap_phase = fourier_phase(bootstrap_signal, sampling_rate, frequency, bandwidth);
+    bootstrap_phase = get_phase(bootstrap_signal, sampling_rate, frequency, bandwidth);
     bootstrap_phase = unwrap(bootstrap_phase((n_burn + 1):(end-n_burn)));
-    statistic(boot) = max(abs(weighted_cusum(bootstrap_phase)));
+    statistic(boot) = max(abs(pd(bootstrap_phase)));
 end
 
 critical_value = quantile(statistic, 1 - alpha);
