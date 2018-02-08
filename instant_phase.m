6e36dc9e331ca51e2ca0e2d7fac241d48806c596
slipperyhank <wmarshall3@wisc.edu>
2018-02-08 10:04:47 -0500

    add comments

--- instant_phase.m
+++ instant_phase.m
@@ -1,14 +1,27 @@
-function [phase, magnitude] = instant_phase(signal, sampling_rate, frequency, bandwidth)
+function [phase, magnitude] = instant_phase(signal, sampling_rate, frequency, delta)
+% Estimate the instantaneous phase and magnitude of a time series using
+% complex demodulation algorithm.
+% Inputs:
+%       signal (vector(float)): observed time series
+%       sampling_rate (int): Temporal scale of time series in
+%           samples-per-second
+%       frequency (float): Frequency of the oscillator of interest
+%       delta (float): Cutoff of lowpass filter
+%
+% Outputs:
+%       phase (vector(float)): time series of instantaneous phase values
+%       magnitude (vector(float)): time series of instantaneous magnitude
+%           values
 
-% Estimate the instantaneous phase of a time series `signal`
-% in the frequency band centred at `frequency` with size 2 * bandwidth
+% Note: instantaneous phase is not unwrapped, and no initial period is
+% "burned" away. 
 
-% phase is not unwrapped, and no initial period is "burned"
+% Note: Filter is implemented as a fourth order butterworth filter.
 
 n_samples = length(signal);
 t = 0:(n_samples - 1);
 
-[b, a] = butter(4, 2 * bandwidth / sampling_rate, 'low');
+[b, a] = butter(4, 2 * delta / sampling_rate, 'low');
 
 inphase = signal .* sin(2 * pi * frequency / sampling_rate * t);
 quadrature = signal .* cos(2 * pi * frequency / sampling_rate * t);
