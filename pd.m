678c2ce9872ffbadf17ef94b2871b93d11724db8
slipperyhank <wmarshall3@wisc.edu>
2018-02-16 14:04:29 -0500

    remove z-scoring of pd

--- pd.m
+++ pd.m
@@ -1,4 +1,9 @@
 function statistic = pd(phase)
+% Calculate the PD statistic from a time series
+% Inputs:
+%   signal (array(float)) - signal of interest
+%
+% Outputs:
+%   statistic (array(float)) - PD statistic
 
 statistic = [0, diff(phase)];
-statistic = (statistic - mean(statistic)) ./ std(statistic);
