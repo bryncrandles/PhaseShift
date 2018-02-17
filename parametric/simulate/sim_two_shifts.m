b0d9167209d970a84b474694a64a70a0642e7bc9
slipperyhank <wmarshall3@wisc.edu>
2018-02-17 08:51:50 -0500

    Add function to simulate with two shift events

--- parametric/simulate/sim_two_shifts.m
+++ parametric/simulate/sim_two_shifts.m
@@ -1,39 +1,34 @@
-function x = sim_two_shifts(T, sRate, SNR, freq, phi, delta_phi, t0)
+function signal = sim_two_shifts(n_samples, sampling_rate, SNR, frequency, phi, shift_magnitude, shift_latency)
+% Simulate observations from a noisy oscillator with two phase shift
+% events. Noise is i.i.d normally distributed. 
+% Inputs:
+%   n_samples (int) - The number of samples in the signal
+%   sampling_rate (int) - Sampling rate of the observations
+%   SNR (float) - Signal-to-noise ratio
+%   frequency (float) - Frequency of the oscillator
+%   phase (float) - Initial phase of the oscillator
+%   shift_magnitude (array(float)) - Two element vector of shift magnitudes 
+%   shift_latency (array(int)) - Two element vector of shift latencies. 
+%           The latencies should correspond to the magnitudes. E.g.,
+%           shift_magnitude(1) is the magnitude of the shift at 
+%           shift_latency(1). 
+%
+% Outputs:
+%   signal (array(float)) - The simulated time series
 
-if nargin < 5
-    error('Not enough parameters')
-elseif nargin < 7
-    delta_phi = 0;
-    t0 = 0;
-end
+% Ensure that the shift events appear in order. 
+[~, index] = sort(shift_latency);
+shift_latency = shift_latency(index);
+shift_magnitude = shift_magnitude(index);
 
-% PARAMETERS
+t = 0:(n_samples - 1);
 
-% T: The number of samples in the signal
+signal = sin(frequency * 2 * pi * t / sampling_rate + phi);
+signal(shift_latency(1):end) = sin(frequency * 2 * pi * t(shift_latency(1):end) / sampling_rate + (phi + shift_magnitude(1)));
+signal(shift_latency(2):end) = sin(frequency * 2 * pi * t(shift_latency(2):end) / sampling_rate + (phi + shift_magnitude(1) + shift_magnitude(2)));
+signal = signal ./ norm(signal);
 
-% sRate: sampling rate, typically 250 samples per second
-
-% freq: frequency of the ocsillator
- 
-% SNR: singal-to-noise ratio: 1 for no noise
-
-% phi: the initial phase of the oscillator
-% delta_phi: the change in phase of the oscillator
-
-% t0: the temporal index of the change point
-
-t=0:(T-1);
-
-x=sin(freq*2*pi*t/sRate+phi);
-
-if t0 > 0
-    x(t0:end) = sin(freq*2*pi*t(t0:end)/sRate+(phi + delta_phi));
-end
-       
-epsilon = randn(1,T); % normal noise
-%epsilon = rand(1,T); % uniform noise
-
-x = x ./ norm(x);
+epsilon = randn(1, n_samples); 
 epsilon = epsilon ./ norm(epsilon);
 
-x = SNR .* x + (1 - SNR) .* epsilon;
+signal = SNR .* signal + (1 - SNR) .* epsilon;
